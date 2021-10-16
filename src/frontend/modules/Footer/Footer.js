b6caaff8a6d5b6d3234a23433717000e9e04b92b
Ikram-hossen <dcseikramhossen1392@gmail.com>
2021-10-17 02:20:09 +0600

    second commit

--- src/frontend/modules/Footer/Footer.js
+++ src/frontend/modules/Footer/Footer.js
@@ -1,64 +1,100 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import FooterLogo from '../../../dist/images/logo.png'
+import FooterIcon from '../../../dist/images/logo-icon.png'
+
 
 const Footer = () => {
     return (
         <footer id="footer">
-        <div className="container-fluid footer-container">
+        <div className="container footer-container">
             <div className="footer-top">
-                <div className="row">
-                    <div className="col-lg-5 col-md-6">
-                        <div className="footer-widget logo-widget">
-                            <Link to="/">
-                                {/* <img src={Logo} alt="Logo-img"/> */}
-                            </Link>
-                            <div className="footer-social">
-                                <ul className="social-list d-flex">
-                                    <li><Link to="/"><i class="fab fa-facebook-f"></i></Link></li>
-                                    <li><Link to="/"><i className="fab fa-instagram"></i></Link></li>
-                                    <li><Link to="/"><i className="fab fa-twitter"></i></Link></li>
-                                    <li><Link to="/"><i className="fab fa-youtube"></i></Link></li>
-                                    <li><Link to="/"><i className="fab fa-whatsapp"></i></Link></li>
-                                    <li><Link to="/"><i className="fab fa-pinterest"></i></Link></li>
-                                </ul>
+                    <div className="row footer-top-row1">
+                    <div className="col-md-6">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="footer-widget">
+                                    <h3>About CWE </h3>
+                                    <ul className="footer-link">
+                                        <li><Link to="/">Careers at</Link></li>
+                                        <li><Link to="/">Customer support</Link></li>
+                                        <li><Link to="/">Blog</Link></li>
+                                            <li><Link to="/">Careers at</Link></li>
+                                            <li><Link to="/">Customer support</Link></li>
+                                            <li><Link to="/">Blog</Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="footer-widget">
+                                    <h3 className="hide">service</h3>
+                                    <ul className="footer-link">
+                                            <li><Link to="/">Booking terms</Link></li>
+                                            <li><Link to="/">Privacy policy</Link></li>
+                                            <li><Link to="/">Website terms</Link></li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-7">
-                        <div className="row">
-                        <div className="col-lg-4 col-md-6">
-                        <div className="footer-widget">
-                            <h3>About Chuzeday</h3>
-                            <ul className="footer-link">
-                                <li><Link to="/">Careers at</Link></li>
-                                <li><Link to="/">Customer support</Link></li>
-                                <li><Link to="/">Blog</Link></li>
-                            </ul>
+                        <div className="col-md-6">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="footer-widget">
+                                        <h3>Services</h3>
+                                        <ul className="footer-link">
+                                            <li><Link to="/">Parcel Delivery</Link></li>
+                                            <li><Link to="/">Warehousing</Link></li>
+                                            <li><Link to="/">Bulk Shipment</Link></li>
+                                            <li><Link to="/">Line Houl</Link></li>
+                                            <li><Link to="/">Logistics Services</Link></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="footer-widget">
+                                        <h3 className="hide">service</h3>
+                                        <ul className="footer-link">
+                                            <li><Link to="/">Logistics Services</Link></li>
+                                            <li><Link to="/">Customizable Solution</Link></li>
+                                            <li><Link to="/">Line Houl</Link></li>
+                                            <li><Link to="/">Warehousing</Link></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                </div>
+                <div className="row footer-top-row2">
+                    <div className="col-md-6">
+                        <ul className="left-inner d-flex align-items-center">
+                            <li><Link to="/"><img src={FooterLogo} alt="logo"/></Link></li>
+                            <li className="d-flex align-items-center">
+                                <span>Call Us:</span>
+                                <Link>0170000000</Link>
+                            </li>
+                            <li className="d-flex align-items-center">
+                                <span>Email:</span>
+                                <Link>CWC@gmail.com</Link>
+                            </li>
+                        </ul>
                     </div>
-                    <div className="col-lg-4 col-md-6">
-                        <div className="footer-widget">
-                            <h3>For business</h3>
-                            <ul className="footer-link">
-                                <li><Link to="/">For Partners</Link></li>
-                                <li><Link to="/">Pricing</Link></li>
-                                <li><Link to="/">Support for partners</Link></li>
+                    <div className="col-md-6">
+                        <div className="right-inner d-flex align-items-center">
+                            <Link to="/"><img src={FooterIcon} alt="img" /></Link>
+                            <ul className="footer-social d-flex align-items-center">
+                                <li><Link to="/"><i className="fab fa-facebook"></i></Link></li>
+                                <li><Link to="/"><i className="fab fa-instagram"></i></Link></li>
+                                <li><Link to="/"><i className="fab fa-linkedin"></i></Link></li>
                             </ul>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-md-6">
-                        <div className="footer-widget">
-                            <h3>Legal</h3>
-                            <ul className="footer-link">
-                                <li><Link to="/">Booking terms</Link></li>
-                                <li><Link to="/">Privacy policy</Link></li>
-                                <li><Link to="/">Website terms</Link></li>
-                            </ul>
-                        </div>
-                    </div>
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="footer-bottom d-flex align-items-center">
+                <span>Copyright <span className="cpy-symbol">&copy;</span> 2021 CWC. All rights reserved.</span>
+                <Link to="/">Privacy Policy</Link>
+                <Link to="/">Terms and Condition</Link>
             </div>
         </div>
     </footer>
--- src/frontend/modules/Header/Header.js
+++ src/frontend/modules/Header/Header.js
@@ -1,6 +1,4 @@
 
-import { Link, NavLink } from "react-router-dom";
-import { Navbar, Nav} from 'react-bootstrap';
 import '../../../dist/css/style.css';
 import '../../../dist/css/responsive.css';
 
@@ -9,30 +7,7 @@ const Header = () => {
 
     return (
         <header id="header" className="header-main">
-            <div className="container-fluid navbarWrap">
-                <Navbar expand="lg">
-                    <NavLink to="/home" className="logo">
-                        {/* <img src={Logo} alt="Logo-img"/> */}
-                    </NavLink>
-                        <Nav
-                            className="ms-auto my-2 my-lg-0"
-                            style={{ maxHeight: '100px' }}
-                            navbarScroll
-                        >
-                            <ul className="primaryMenu">
-                                <li><NavLink className="navItem" activeClassName="navItemActive" to="/">Home</NavLink></li>
-                                <li><NavLink className="navItem" to="/About">About</NavLink></li>
-                                <li><NavLink className="navItem" to="/partnerprofile">For Partners</NavLink></li>
-                                <li className="dropdownWrap"><NavLink className="navItem" to="/">Log In</NavLink>
-                                <ul className="submenu">
-                                    <li><NavLink to="/partnerlogin">Partner</NavLink></li>
-                                    <li><NavLink to="/customerlogin">Guests</NavLink></li>
-                                </ul>
-                                </li>
-                            </ul>
-                        </Nav>
-                </Navbar>
-                </div>
+            Header
         </header>
      );
 }
